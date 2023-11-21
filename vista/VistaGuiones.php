4f64dd1015d5ddb96a2f4f7e7f73dd360925fc8b
Carlos Angulo <angulo.navas@gmail.com>
2023-11-21 16:58:14 +0100

    limpiando master de guiones

--- vista/VistaGuiones.php
+++ vista/VistaGuiones.php
@@ -23,7 +23,7 @@ CUERPO DE LA VISTA
 		<h1>Guiones</h1>
 	</div>
 
-	<!-- se define la vista activa q5 -->
+	<!-- se define la vista activa -->
 	<?php $vistaActiva = 'VistaGuiones'; ?>
 
 	<!-- menú principal de la aplicación -->
